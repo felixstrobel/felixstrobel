9bdafe70ef36b25d07abd6efc1b2491d44f59327
Felix Strobel <main.felixstrobel@gmail.com>
2021-06-22 17:02:46 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    purge: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        colors: {
+            dark: '#0D1117',
+            light: '#C9D1D9',
+        },
+        fontFamily:{
+            goldman:['Goldman'],
+        },
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
